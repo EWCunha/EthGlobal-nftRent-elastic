9f13630a17da6dd2bb1cf2113f3001e392add8b6
GZRdev <gloriareilly@protonmail.com>
2022-07-13 16:22:58 -0700

    Testing stuff. Trying to add stuff from list to show on search.

--- frontend/src/components /List.js
+++ frontend/src/components /List.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { Box, Button, Typography, Card, Grid, TextField } from '@mui/material'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const List = () => {
+
+  const defaultAccount = useSelector((state) => state.defaultAccount)
+  const contract = useSelector((state) => state.contract)
 
   const [benefits, setBenefits] = useState(null);
   const [rentPerDay, setRentPerDay] = useState(null);
@@ -43,71 +46,82 @@ const List = () => {
   const dispatch = useDispatch()
 
   const listNFT= () => {
-    dispatch({type:"ENTER_DESCRIPTION", payload:benefits})
+    contract.listNFT(
+      nFTAddress,
+       tokenId,
+       rentPerDay,
+       collateralDeposit,
+       benefits
+      )
   }
 
   return (
 
-    <Grid container item spacing = { 2 }>
+    
+     defaultAccount ? (
+      
+      <Grid container item spacing = { 2 }>
 
-      <Grid container item xs={ 12 } sm={ 12 } md={ 12 }>
-        <Typography>
-          LIST AN NFT
-        </Typography>
-      </Grid>
+        <Grid container item xs={ 12 } sm={ 12 } md={ 12 }>
+          <Typography>
+            LIST AN NFT
+          </Typography>
+        </Grid>
 
-      <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
-        <TextField label="Details benefits"
-        variant="outlined"
-        onChange={(e) => setBenefits(e.target.value)}        
-        >
-        </TextField>
-      </Grid>
-
-      <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
-        <TextField label="Rent per day"
-        variant="outlined"
-        onChange={(e) => setRentPerDay(e.target.value)}         
-        >
-        </TextField>
-      </Grid>
-
-      <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
-        <TextField label="Collateral deposit"
-        variant="outlined"
-        onChange={(e) => setCollateralDeposit(e.target.value)}         
-        >
-        </TextField>
-      </Grid>
-
-      <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
-        <TextField label="NFT address"
-        variant="outlined"
-        onChange={(e) => setNFTAddress(e.target.value)}        
-        >
-        </TextField>
-      </Grid>
-
-      <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
-        <TextField label="Token id"
-        variant="outlined"
-        onChange={(e) => setTokenId(e.target.value)}        
-        >
-        </TextField>
-      </Grid>
-
-      <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
-        <Box>
-          <Button variant="contained"
-            color="error"
-            onClick={listNFT}
+        <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
+          <TextField label="Details benefits"
+          variant="outlined"
+          onChange={(e) => setBenefits(e.target.value)}        
           >
-            LIST NFT
-          </Button>
-        </Box>
+          </TextField>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
+          <TextField label="Rent per day"
+          variant="outlined"
+          onChange={(e) => setRentPerDay(e.target.value)}         
+          >
+          </TextField>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
+          <TextField label="Collateral deposit"
+          variant="outlined"
+          onChange={(e) => setCollateralDeposit(e.target.value)}         
+          >
+          </TextField>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
+          <TextField label="NFT address"
+          variant="outlined"
+          onChange={(e) => setNFTAddress(e.target.value)}        
+          >
+          </TextField>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
+          <TextField label="Token id"
+          variant="outlined"
+          onChange={(e) => setTokenId(e.target.value)}        
+          >
+          </TextField>
+        </Grid>
+
+        <Grid item xs={ 12 } sm={ 12 } md={ 12 }>
+          <Box>
+            <Button variant="contained"
+              color="error"
+              onClick={listNFT}
+            >
+              LIST NFT
+            </Button>
+          </Box>
+        </Grid>
+
       </Grid>
 
-    </Grid>
+    ) : <div>Please connect your wallet to list an NFT.</div>
   )
 }
 
--- frontend/src/components /Web3Connect.js
+++ frontend/src/components /Web3Connect.js
@@ -9,40 +9,40 @@ const Web3Connect = () => {
     const ABI = contractJSON.abi
     const ADDRESS = contractJSON.address
 
-const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-const [connButtonText, setConnButtonText] = useState('Connect Wallet');
-const [accountchanging, setAccountChanging] = useState(false)
-const [errorMessage, setErrorMessage] = useState(null);
-const [connectButtonColor, setConnectButtonColor] = useState("secondary")
+    const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+    const [accountchanging, setAccountChanging] = useState(false)
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [connectButtonColor, setConnectButtonColor] = useState("secondary")
 
-const [defaultAccount, setDefaultAccount] = useState(null)
-const [provider, setProvider] = useState(null)
-const [signer, setSigner] = useState(null)
-const [walletBalance, setWalletBalance] = useState(null)
+    const [defaultAccount, setDefaultAccount] = useState(null)
+    const [provider, setProvider] = useState(null)
+    const [signer, setSigner] = useState(null)
+    const [walletBalance, setWalletBalance] = useState(null)
 
-const connectWalletHandler = () => {
-    if (window.ethereum && window.ethereum.isMetaMask) {
-        console.log("CONNECTING TO WALLET")
-        window.ethereum.request({ method: 'eth_requestAccounts' })
-            .then(result => {
+    const connectWalletHandler = () => {
+        if (window.ethereum && window.ethereum.isMetaMask) {
+            console.log("CONNECTING TO WALLET")
+            window.ethereum.request({ method: 'eth_requestAccounts' })
+                .then(result => {
 
-                accountChangedHandler(result[0]);
-                setConnButtonText('Wallet Connected');
-                setConnectButtonColor("success")
+                    accountChangedHandler(result[0]);
+                    setConnButtonText('Wallet Connected');
+                    setConnectButtonColor("success")
 
 
 
-            })
-            .catch(error => {
-                setErrorMessage(error.message);
+                })
+                .catch(error => {
+                    setErrorMessage(error.message);
 
-            });
+                });
 
-    } else {
-        console.log('Need to install MetaMask');
-        setErrorMessage('Please install MetaMask browser extension to interact');
-    }
+        } else {
+            console.log('Need to install MetaMask');
+            setErrorMessage('Please install MetaMask browser extension to interact');
+        }
 }
 
 const accountChangedHandler = (newAccount) => {
