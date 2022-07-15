b29d3fdbc0287dafbc09fba8413a927b33f6feaa
Kit Fuderich <kitfuderich@gmail.com>
2022-07-15 15:50:16 -0700

    search table implemented along with model prompting user to rent selected NFT

---
+++ frontend/src/components/RentalModal.js
@@ -0,0 +1,46 @@
+import React,{useState} from 'react'
+import{
+TextField,
+Button,
+Modal,
+Box
+  } from '@mui/material'
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+const RentalModal = ({open,handleClose,handleRentalDays,completeRental}) => {
+  return (
+    <>
+    <Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+  <div>How many days to rent?</div>
+    <TextField onChange={(e)=>handleRentalDays(e.target.value)}></TextField>
+    <Button 
+    variant="contained"
+    onClick={completeRental}
+    >COMPLETE RENTAL</Button>
+  </Box>
+   
+  
+  </Modal>
+    </>
+  )
+
+}
+
+export default RentalModal
